fa14e48b5e7a2cd07b25dff0c31d4d6496713154
Arief Darmawan <121046920+ariefdrm@users.noreply.github.com>
2023-09-28 23:38:21 +0700

    Delete Looping/while/table-penelusuran.js

--- Looping/while/table-penelusuran.js
+++
@@ -1,9 +0,0 @@
-// cara pakai table penelusuran / trace table
-
-let jmlhAngkot = 100;
-let angkot = 1;
-
-while (angkot <= jmlhAngkot) {
-    console.log ('Angkot no.' + angkot + ' Beroperasi dengan baik');
-angkot++;
-}
